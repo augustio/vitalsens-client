571fba5e66067e05ec71f2ee12c709da037a366c
augustio <augustine.onubeze@metropolia.fi>
2016-08-22 13:17:35 +0300

    More on user authentication

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -1,21 +1,31 @@
 export class AuthController{
 
-    constructor($auth){
+    constructor($auth, $log, $state){
         'ngInject';
 
         this.$auth = $auth;
+        this.$log = $log;
+        this.$state = $state;
     }
     register(){
         var vm = this;
-        this.$auth.signup(this.user).then(function(token){
-            vm.$auth.setToken(token);
+        this.$auth.signup(this.user).then(function(response){
+            vm.$log.log(response);
+            vm.$auth.setToken(response.data.token);
+            vm.$state.go('home');
+        }).catch(function(response){
+            vm.$log.log(response);
         });
     }
     
     login(){
         var vm = this;
-        this.$auth.login(this.login.user).then(function(token){
-            vm.$auth.setToken(token);
+        this.$auth.login(this.login.user).then(function(response){
+            vm.$log.log(response);
+            vm.$auth.setToken(response.data.token);
+            vm.$state.go('home');
+        }).catch(function(response){
+            vm.$log.log(response);
         });
     }
         
