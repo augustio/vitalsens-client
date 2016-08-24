edb3d4eed14fad32ababcb408b0eb846cc532e09
augustio <augustine.onubeze@metropolia.fi>
2016-08-24 11:02:16 +0300

    Update API_URL with cloud server url

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -17,7 +17,7 @@ import { NavbarDirective } from '../app/components/navbar/navbar.directive';
 import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 
 angular.module('vitalsens', ['ui.router', 'ui.bootstrap', 'toastr', 'satellizer'])
-  .constant('API_URL', 'http://localhost:5000/')
+  .constant('API_URL', 'http://52.210.133.58:5000/')
   .constant('malarkey', malarkey)
   .constant('moment', moment)
   .config(config)
