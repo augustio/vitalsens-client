c6290ea544e4a88ee696ff196f1d4aa3ab845e3b
augustio <augustine.onubeze@metropolia.fi>
2016-08-24 11:08:40 +0300

    Replace localhost with cloud server url constant

--- src/app/record/controllers/record.controller.js
+++ src/app/record/controllers/record.controller.js
@@ -1,14 +1,15 @@
 export class RecordController {
-  constructor ($http) {
+  constructor ($http, API_URL) {
       'ngInject';
 
       this.$http = $http;
+      this.API_URL = API_URL;
       this.getRecords();
   }
 
     getRecords(){
         var vm = this;
-        this.$http.get('http://localhost:5000/api/records').then(function(result){
+        this.$http.get('http://'+this.API_URL+':5000/api/records').then(function(result){
             vm.records = result.data;
         });
     }
--- src/app/record/controllers/record.detail.controller.js
+++ src/app/record/controllers/record.detail.controller.js
@@ -1,9 +1,10 @@
 export class RecordDetailController {
-  constructor ($http, $state) {
+  constructor ($http, $state, API_URL) {
       'ngInject';
 
       this.$http = $http;
       this.$state = $state;
+      this.API_URL = API_URL;
       this.getRecordDetail();
   }
 
@@ -12,7 +13,7 @@ export class RecordDetailController {
         var tStamp = this.$state.params.timeStamp;
         var pId = this.$state.params.patientId;
         var tp = this.$state.params.type;
-        this.$http.get('http://localhost:5000/api/record-details?timeStamp='+tStamp+'&patientId='+pId+'&type='+tp).then(function(result){
+        this.$http.get('http://'+this.API_URL+':5000/api/record-details?timeStamp='+tStamp+'&patientId='+pId+'&type='+tp).then(function(result){
             vm.detail = result.data;
         });
     }
