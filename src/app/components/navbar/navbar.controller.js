b1fd69209a2853e901aefa1eb7de6a7a5243e0be
augustio <augustine.onubeze@metropolia.fi>
2016-10-20 20:19:23 +0300

    Initial implementation of access control

--- src/app/components/navbar/navbar.controller.js
+++ src/app/components/navbar/navbar.controller.js
@@ -11,4 +11,13 @@ export class NavbarController{
         this.$auth.logout();
         this.$state.go('home');
     }
+    
+    isAdmin(){
+        return this.$auth.getPayload().user.role === "admin";
+    }
+    
+    getUser(){
+        this.userEmail = this.$auth.getPayload().user.email;
+        return this.userEmail != null;
+    }
 }
