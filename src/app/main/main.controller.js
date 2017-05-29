655156a35e019107ff6b03c5698f16236f9d4caa
augustio <augustine.onubeze@metropolia.fi>
2017-05-30 02:41:11 +0300

    Implement main controller and view to list patient's records with filters

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,12 +1,58 @@
 export class MainController {
-  constructor($auth, $state){
+  constructor($auth, $state, $http, API_URL){
     'ngInject';
 
     this.$auth = $auth;
     this.$state = $state;
+    this.$http = $http;
+    this.API_URL = API_URL;
 
     this.authUser = this.$auth.getPayload().user;
     let token = this.$auth.getToken();
     if(!token) { this.$state.go('login'); }
+
+    this.filters = {};
+    this.selectedPatient = null;
+    this.getPatients();
+  }
+
+  getPatients(){
+    this.$http.get(this.API_URL+'api/users/patients')
+      .then(successRes => {
+        this.patients = successRes.data.users;
+      });
+  }
+
+  getPatientRecords(){
+    if(!this.selectedPatient) { return; }
+    this.$http.get(this.API_URL+'api/records/user/'+this.selectedPatient.userId)
+      .then(successRes => {
+        this.records = successRes.data || [];
+        this.filteredRecords = this.records;
+      });
+  }
+
+  filterRecords(){
+    this.filteredRecords = this.records;
+    if(this.filters.type){
+      this.filteredRecords = this.records.filter(r => r.type === this.filters.type);
+    }
+    if(this.filters.dateFrom){
+      let from = moment(this.filters.dateFrom).unix()*1000;
+      this.filteredRecords = this.filteredRecords.filter(r => {
+        return r.recStart >= from;
+      });
+    }
+    if(this.filters.dateTo){
+      let to = moment(this.filters.dateTo).unix()*1000;
+      this.filteredRecords = this.filteredRecords.filter(r => {
+        return r.recStart <= to;
+      });
+    }
+  }
+
+  getDuration(from, to){
+    if(to < from) { return 0; }
+    return (to - from)/60000;
   }
 }
